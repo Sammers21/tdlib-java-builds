53e3d186233b8e6bafcc065fef60bcb6287ef3fd
Sammers21 <titantins@gmail.com>
2020-01-04 02:42:35 +0300

    add mac os build file

---
+++ macos.sh
@@ -0,0 +1,17 @@
+git clone https://github.com/tdlib/td.git
+cd td
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/ -DCMAKE_INSTALL_PREFIX:PATH=../example/java/td -DTD_ENABLE_JNI=ON ..
+cmake --build . --target install
+cd ..
+cd example/java
+rm -rf build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../../../tdlib -DTd_DIR:PATH=$(greadlink -e ../td/lib/cmake/Td) ..
+cmake --build . --target install
+cd ../../..
+cd ..
+ls -l td/tdlib
