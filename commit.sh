c4a4fd226c47204e97a9afe8ed4df6f46d22da4a
Sammers21 <titantins@gmail.com>
2020-01-09 23:00:40 +0300

    improve commit

--- commit.sh
+++ commit.sh
@@ -2,6 +2,7 @@ set -e
 set -x
 cp -a td/tdlib/bin/. ./
 rm -rf td/
+git stash && git pull && git stash pop
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git checkout master
