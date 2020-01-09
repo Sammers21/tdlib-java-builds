1b93c7e3185ed0aed43e950ff49c84efda2adf86
Sammers21 <titantins@gmail.com>
2020-01-09 22:14:39 +0300

    improve commit script

--- commit.sh
+++ commit.sh
@@ -5,4 +5,4 @@ rm -rf td/
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git checkout master
-git commit -m "Update binaries" -a
+git commit -m "Update binaries" -a || true
